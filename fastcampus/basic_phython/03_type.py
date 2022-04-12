40f4572c21d34e18698412bee8d968c5bc6c1147
jexists <jexists486@gmail.com>
2022-04-12 22:23:42 +0900

    숫자형

---
+++ fastcampus/basic_phython/03_type.py
@@ -0,0 +1,18 @@
+
+int_a = 1
+int_b = -1
+float_a = 0.1
+float_b = 0.0
+float_c = -0.1
+float_d = -0.
+str_a = 'hello'
+
+print(type(int_a)) # <class 'int'>
+print(type(int_b)) # <class 'int'>
+print(type(float_a)) # <class 'float'>
+print(type(float_b)) # <class 'float'>
+print(type(float_c)) # <class 'float'>
+print(type(float_d)) # <class 'float'>
+# int + float = float
+print(type(int_a + float_d)) # <class 'float'>
+print(type(str_a)) # <class 'str'>
---
+++ fastcampus/basic_phython/04_operator.py
@@ -0,0 +1,35 @@
+
+a = 10
+b = 3
+
+print(a + b) # 더하기 = 13
+print(a - b) # 빼기 = 7
+print(a * b) # 곱하기 = 30
+print(a / b) # 나누기 = 3.3333333333333335 
+print(a // b) # 몫 = 3
+print(a % b) # 나머지 = 1
+print(a ** b) # 제곱 = 1000
+
+print("할당연산===")
+# 할당연산
+z = 1
+z = z + 1 
+print(z) # 2
+z = 1
+z += 1
+print(z) # 2
+
+z = 1
+z = z - 1 
+print(z) # 0
+z = 1
+z -= 1
+print(z) # 0
+
+z = 2
+z *= 2
+print(z) # 4
+
+z = 1
+z /= 2
+print(z) # 0.5
