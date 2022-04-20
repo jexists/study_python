9f374b368ed485e7ecfb442776ae38596748370f
jexists <jexists486@gmail.com>
2022-04-20 23:49:33 +0900

    리스트 타입

--- fastcampus/basic_phython/03_type.py
+++ fastcampus/basic_phython/03_type.py
@@ -1,4 +1,6 @@
 
+# 숫자형
+
 int_a = 1
 int_b = -1
 float_a = 0.1
@@ -47,3 +49,63 @@ str_e = 'hello world'
 print(str_e.split()) # ['hello', 'world'] // space 기준 분리
 str_f = 'joy, happy, sadness'
 print(str_f.split(',')) # ['joy', ' happy', ' sadness'] // 기준 분리
+
+
+# 리스트
+
+list_a = [1, 2, 3, 4, 5]
+print(list_a) # [1, 2, 3, 4, 5]
+print(type(list_a)) # <class 'list'>
+
+# 리스트내부에 리스트 포함 가능
+list_b = [1, [1, 2], [1, [1, 2]]] 
+print(list_b[0]) # 1
+print(list_b[1][1]) # 2
+print(list_b[2][1][0]) # 1
+
+list_c = [1, [2, 3], [4, [5, 6, 7]]] 
+print(list_c[0]) # 1
+print(list_c[-1]) # [4, [5, 6, 7]]
+
+# 슬라이스
+print(list_c[1:]) # [[2, 3], [4, [5, 6, 7]]]
+print(list_c[2][1][1:]) # [6, 7]
+
+# 치환
+list_d = [1, [2, 3], [4, [5, 6, 7]]] 
+
+list_d[0] = 10
+print(list_d) # [10, [2, 3], [4, [5, 6, 7]]]
+list_c[2][1][1:] = [60, 70]
+print(list_c) # [1, [2, 3], [4, [5, 60, 70]]]
+# list_c[2][1][1:] = 50 # ERROR (같은 타입/자료형이여야 함)
+list_c[2][1][1:] = [50]
+print(list_c) # [1, [2, 3], [4, [5, 50]]]
+
+# 리스트 계산
+a = [1, 2, 3]
+b = [4, 5, 6]
+print(a + b) # [1, 2, 3, 4, 5, 6]
+
+print(a * 2) # [1, 2, 3, 1, 2, 3]
+
+# 내장함수
+# append: 뒤에 값 넣기
+c = [1, 2, 3]
+c.append(4)
+print(c) # [1, 2, 3, 4]
+d = [1, 2, 3]
+d.append([4, 5])
+print(d) # [1, 2, 3, [4, 5]]
+
+# pop: 해당 위치 인덱스 삭제
+e = [1, 2, 3]
+e.pop()
+print(e) # [1, 2]
+f = [1, 2, 3]
+f.pop(1)
+print(f) # [1, 3]
+
+# index: 리스트에 인덱스 값 반환
+g = [1, 2, 3]
+print(a.index(2)) # 1 (인덱스)
