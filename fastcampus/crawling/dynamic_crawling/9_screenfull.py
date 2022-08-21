c2c0dfc829be76e688ab6fdb881672244ec3dbb2
jexists <jexists486@gmail.com>
2022-08-21 22:51:19 +0900

    screenshot

--- fastcampus/crawling/dynamic_crawling/9_screenfull.py
+++ fastcampus/crawling/dynamic_crawling/9_screenfull.py
@@ -11,7 +11,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 # 크롬 옵션 설정
 options = webdriver.ChromeOptions()
-options.add_argument('window-size=1000,1000')
+options.headless = True
 options.add_argument('no-sandbox')
 
 chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
@@ -30,13 +30,15 @@ def finds_visible(css):
 
 find_visible("input#query").send_keys("레사모\n")
 
-
 e = find_visible("li[data-cr-rank='3']")
 # 일부 스크린샷
-e.screenshot("./test.png")
 
-chrome.save_screenshot("./el_screen.png")
-time.sleep(3)
+chrome.save_screenshot("./test9.png")
+chrome.set_window_size(1000,10000)
+chrome.save_screenshot("./test10.png")
 
+# window 사이즈를 넉넉하게 잡아줘야함
+body = find_visible("body")
+body.screenshot("./test11.png")
  
 chrome.close()
