677fe7d1359953523fc9bd0b03638b53f354485f
jexists <jexists486@gmail.com>
2022-09-08 00:45:00 +0900

    한 페이지 크롤링 완료

--- my_practice/crawler/size_bp.py
+++ my_practice/crawler/size_bp.py
@@ -1,4 +1,5 @@
 
+from urllib.request import urlopen
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -15,6 +16,7 @@ import random
 
 from dotenv import load_dotenv
 import os
+import string
 
 import csv
 
@@ -22,12 +24,17 @@ load_dotenv()
 
 # 크롬 옵션 설정
 options = webdriver.ChromeOptions()
-options.add_argument('window-size=1000,1000')
+options.headless = True
+# options.add_argument('window-size=1000,1000')
 options.add_argument('no-sandbox')
 
 chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 chrome.get('https://cafe.naver.com/re4mo')
 wait = WebDriverWait(chrome, 10)
+
+
+def id_generator(size=8, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 def find_presence(css):
     return wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, css)))
@@ -80,23 +87,42 @@ board = finds_visible(".article-board")
 lists = board[1].find_elements(By.CSS_SELECTOR, 'tbody .td_article')
 print(len(lists))
 
+re4mo = [];
 for idx, val in enumerate(lists):
     chrome.implicitly_wait(10)
-    print(idx)
     if idx >= 1:
         chrome.switch_to.frame("cafe_main")
         board = finds_visible(".article-board")
         lists = board[1].find_elements(By.CSS_SELECTOR, 'tbody .td_article')
         chrome.implicitly_wait(10)
+    title = lists[idx].find_element(By.CSS_SELECTOR, '.board-list .article').get_attribute('innerText')
+    link = lists[idx].find_element(By.CSS_SELECTOR, '.board-number .inner_number').get_attribute('innerText')
+    print(idx, title, link)
+
     lists[idx].find_element(By.CSS_SELECTOR, '.board-list .article').click()
     chrome.implicitly_wait(10)
     chrome.switch_to.default_content()
     time.sleep(3)
-    print(chrome.title)
     chrome.switch_to.frame("cafe_main")
     chrome.implicitly_wait(10)
+    time.sleep(5)
 
-    contents = chrome.find_elements(By.CSS_SELECTOR, '.se-main-container')
+    chrome.set_window_size(1000,10000)
+    contents = find_visible('.se-main-container')
+    save_path = str(idx) + "_" + link
+    os.mkdir(save_path)
+    contents.screenshot(save_path + "/" + str(idx)+".png")
+
+    content = contents.get_attribute('innerText').strip()
+    date = chrome.find_element(By.CSS_SELECTOR, '.article_info .date').get_attribute('innerText')
+    nickname = chrome.find_element(By.CSS_SELECTOR, '.nick_box .nickname').get_attribute('innerText')
+
+    imgs = contents.find_elements(By.CSS_SELECTOR, 'img')
+    for img in imgs:
+        src = img.get_attribute('src')
+        t = urlopen(src).read()
+        file = open(os.path.join(save_path, link + id_generator() + ".jpg"), "wb")
+        file.write(t)
 
     chrome.implicitly_wait(10)
     chrome.back()
@@ -104,9 +130,21 @@ for idx, val in enumerate(lists):
     chrome.switch_to.default_content()
     chrome.implicitly_wait(10)
 
-# chrome.switch_to.frame("cafe_main")
+    temp = []
+    temp.append(link)
+    temp.append(title)
+    temp.append(date)
+    temp.append(nickname)
+    temp.append(content)
+    re4mo.append(temp) #list 안에 list 가 들어가는 형태
+
 time.sleep(3)
 
 chrome.quit()
 
+f = open(f're4mo_size.csv','w',encoding='utf-8',newline='') #파일오픈
+csvWriter = csv.writer(f)#열어둔 파일
+for i in re4mo:
+    csvWriter.writerow(i) 
 
+f.close()
