995d29170a22534ada3bdde05ec1251695302f59
jexists <jexists486@gmail.com>
2022-11-12 21:04:43 +0900

    알고리즘

---
+++ algorism/baekjoon/11654.py
@@ -0,0 +1,9 @@
+
+
+input = input()
+ascii = ord(input)
+print(ascii)
+string = chr(ascii)
+print(string)
+
+# print(ord(input()))
---
+++ algorism/baekjoon/11720.py
@@ -0,0 +1,12 @@
+
+num = int(input())
+list = input()
+total = 0
+for item in list:
+    total += int(item)
+print(total)
+
+# 참고
+n = input()
+# del sum
+print(sum(map(int,input())))
