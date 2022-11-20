08d7217d7031b7bbd12d9212da02ca4f4362c9ba
jexists <jexists486@gmail.com>
2022-11-21 01:16:39 +0900

    플레이어

--- my_practice/crawling_final/player_img_final.py
+++ my_practice/crawling_final/player_img_final.py
@@ -7,21 +7,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
-import time
 from webdriver_manager.chrome import ChromeDriverManager
-import pyperclip # 임시 저장소
 
 import random
-
-import re
-
-from dotenv import load_dotenv
-import os
 import string
-
 import csv
 
-load_dotenv()
 
 # 크롬 옵션 설정
 options = webdriver.ChromeOptions()
@@ -30,7 +21,9 @@ options.add_argument('window-size=1000,1000')
 options.add_argument('no-sandbox')
 
 chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-chrome.get('https://www.fifa.com/fifaplus/ko/tournaments/mens/worldcup/qatar2022/teams/belgium/squad')
+
+# 파일 이름 (링크) ################
+chrome.get('https://www.fifa.com/fifaplus/ko/tournaments/mens/worldcup/qatar2022/teams/netherlands/squad')
 wait = WebDriverWait(chrome, 10)
 
 
@@ -53,7 +46,6 @@ def finds_visible(css):
 
 
 board = finds_visible(".player-badge-card_badgeCard__2DJ4B")
-print(len(board))
 
 re4mo = []
 for idx, val in enumerate(board):
@@ -61,23 +53,13 @@ for idx, val in enumerate(board):
     title = board[idx].find_element(By.CSS_SELECTOR, '.player-badge-card_playerFirstname__1HlN6').get_attribute('innerText')
     position = board[idx].find_element(By.CSS_SELECTOR, '.player-badge-card_playerPosition__wjnoI').get_attribute('innerText')
 
-    # print(title)
-
-    # chrome.set_window_size(1000,10000)
-    # contents = find_visible('.se-main-container')
-    save_path = "./argentina"
-    # os.mkdir(save_path)
-    # contents.screenshot(save_path + "/" + str(idx)+".png")
+    # csv 저장하는 위치
+    save_path = "./"
 
     imgs = board[idx].find_elements(By.CSS_SELECTOR, '.player-badge-card_playerImage__301X0')
 
     for img in imgs:
         src = img.value_of_css_property("background-image")
-        print(title, ' ', position)
-        print("\"photo_img\" :"+ src + ',',)
-        # t = urlopen(src.lstrip('url("').rstrip('")')).read()
-        # file = open(os.path.join(save_path, title + ".png"), "wb")
-        # file.write(t)
 
         temp = []
         temp.append(title)
@@ -88,7 +70,8 @@ for idx, val in enumerate(board):
 chrome.quit()
 
 
-f = open(f'belgium.csv','w',encoding='utf-8-sig',newline='') #파일오픈
+# 파일 이름 (나라) ################
+f = open(f'netherlands.csv','w',encoding='utf-8-sig',newline='') #파일오픈
 csvWriter = csv.writer(f)#열어둔 파일
 csv_title = ["타이틀", "포지션", "링크"]
 csvWriter.writerow(csv_title)
