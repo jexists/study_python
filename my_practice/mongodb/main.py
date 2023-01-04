ec8f5aec76c69dd6e58841df37c519d0f32fa1cf
jexists <jexists486@gmail.com>
2023-01-04 22:53:33 +0900

    mongodb insert

--- my_practice/mongodb/main.py
+++ my_practice/mongodb/main.py
@@ -27,3 +27,34 @@ test_db = client.test
 
 collections = test_db.list_collection_names()
 print(collections)
+
+# 삽입하기
+def insert_test_doc():
+  collection = test_db.test
+  test_document = {
+    "name": "joy",
+    "type": "test"
+  }
+  insert_id = collection.insert_one(test_document).inserted_id
+  print(insert_id)
+# insert_test_doc()
+
+production = client.production
+person_collection = production.person_collection
+
+def create_docments():
+  first_names = ["joy", "sad"]
+  last_names = ["J", "H"]
+  ages = [21, 24]
+
+  docs = []
+  for first_name, last_name, age in zip(first_names, last_names, ages):
+    doc = {
+      "first_name": first_name,
+      "last_name": last_name,
+      "age": age,
+    }
+    docs.append(doc)
+  person_collection.insert_many(docs)
+
+create_docments()
